6c72ce74173a4c91e2b0f725ae904ebe65e05bd0
nickeywickey <utahinsurance.nick@gmail.com>
2024-04-04 03:35:20 +0200

    Added HTML pages for navigation through website. Added CRUD operations for User and added User Model.

--- KDRAMA/drama_app/models.py
+++ KDRAMA/drama_app/models.py
@@ -1,4 +1,13 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
+
+class User(models.Model):
+    user_id = models.AutoField(primary_key=True, null=False, validators=[MaxValueValidator(99999999999)])
+    username = models.CharField(max_length=50, null=False)
+    password = models.CharField(max_length=255, null=False)
+    first_name = models.CharField(max_length=50, null=True)
+    last_name = models.CharField(max_length=50, null=True)
+    email = models.CharField(max_length=100, null=True)
 
 class Award(models.Model):
     name = models.CharField(max_length=100)
